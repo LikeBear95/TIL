592576f07c66c9a3bb67842b437849b1680b525b
LikeBear95 <wjdtpwls32@naver.com>
2023-07-19 18:09:21 +0900

    230719

---
+++ my_math.py
@@ -0,0 +1,8 @@
+# book.py
+import test
+number_of_book = 100
+
+
+def decrease_book():
+    print(test.rental.get('number'))
+    print(f'남은 책의 수 : {number_of_book}')
---
+++ test.py
@@ -0,0 +1,11 @@
+# ws_3_3.py
+import my_math
+rental = dict()
+
+def rental_book(name, number):
+    my_math.decrease_book()
+    rental = {'name': name, 'number': number}
+    print(f'{name}님이 {number}권의 책을 대여하였습니다.')
+    return rental
+
+rental_book('홍길동', 3)
