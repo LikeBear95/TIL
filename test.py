50f583261f466712053efeae95796c34282f95a6
LikeBear95 <wjdtpwls32@naver.com>
2023-07-21 12:07:07 +0900

    230721

--- test.py
+++ test.py
@@ -1,31 +1,21 @@
-list_of_book = ['장화홍련전','가락국 신화','온달 설화','금오신화','이생규장전','만복자서포기','수성지','백호집','원생몽유록','홍길동전','장생전','도문대작','옥루몽','옥련몽']
+import json # 내장 모듈
 
-rental_book = ['장생전','위대한 개츠비', '원생몽유록','이생규장전', '데미안', '장화홍련전','수성지','백호집','난중일기','홍길동전','만복자서포기']
+# json 데이터
+json_data = '''
+{
+    "name": "김싸피",
+    "age": 28,
+    "hobbies": [
+        "공부하기",
+        "복습하기"
+    ]
+}
+'''
 
-# is_book = True
+data = json.loads(json_data)
+print(type(data))
 
-# for i in range(len(rental_book)):
-#     for j in range(len(list_of_book)):
-#         if rental_book[i] == list_of_book[j]:
-#             break
-#         else:
-#             if j + 1 == len(list_of_book):
-#                 is_book = False
-#                 print(f'{rental_book[i]} 은/는 보유하고 있지 않습니다.')
-#                 break
-#     if is_book == False:
-#         break
-#     else:
-#         if i + 1 == len(rental_book):
-#             print("모든 도서가 대여 가능한 상태입니다.")
+# JSON 데이터에서 원하는 데이터만 가져오기
+name = data.get('name')
 
-# missing_book = [ if j != ]
-
-missing_book = set(rental_book) - set([ rental_book[i] for i in range(len(rental_book)) for j in range(len(list_of_book)) if rental_book[i] == list_of_book[j] ])
-print(missing_book)
-
-if len(missing_book) == 0:
-    print("모든 도서가 대여 가능한 상태입니다.")
-else:
-    for i in range(len(missing_book)):
-        print(f'{list(missing_book)[i]} 을/를 보충하여야 합니다.')
+print(name)
