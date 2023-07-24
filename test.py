a0d7176b2fa23cb015ffd0d55bffbcb54c36fdef
LikeBear95 <wjdtpwls32@naver.com>
2023-07-24 17:58:22 +0900

    230724

--- test.py
+++ test.py
@@ -1,31 +1,40 @@
-# hw_8_4.py
+# ws_8_5.py
 
 # 아래 클래스를 수정하시오.
-class UserInfo:
+class Animal:
+    num_of_animal = 0 
+
+class Dog(Animal):
+    sound = '멍멍'
     def __init__(self):
-        self.user_data = {}
-    
-    def get_user_info(self):
-        # 이름과 나이를 입력
-        try:
-            user_name = input('이름을 입력하세요: ')
-            user_age = int(input('나이를 입력하세요: '))
-            self.user_data['name'] = user_name
-            self.user_data['age'] = user_age
-        # 잘못된 형식을 입력할 경우
-        except ValueError:
-            print('나이는 숫자로 입력해야 합니다.')
+        Animal.num_of_animal += 1
 
-    def display_user_info(self):
-        # 유저 데이터를 입력 받았는지 확인
-        if len(self.user_data) > 0:
-            print('사용자 정보: ')
-            print(f'이름: {self.user_data["name"]}')
-            print(f'나이: {self.user_data["age"]}')
-        # 유저 데이터가 없다면 오류문구 출력
-        else:
-            print('사용자 정보가 입력되지 않았습니다.')
+    def bark(self):
+        print(f'{self.sound}!')
 
-user = UserInfo()
-user.get_user_info()
-user.display_user_info()
+class Cat(Animal):
+    sound = '야옹'
+    def __init__(self):
+        Animal.num_of_animal += 1
+
+    def meow(self):
+        print(f'{self.sound}!')
+
+class Pet(Dog, Cat):
+    def __str__(self, sound):
+        return f'애완동물은 {self.sound} 소리를 냅니다'
+
+
+    def access_num_of_animal(self):
+        return Animal.num_of_animal
+
+    # def make_sound(self):
+    #     print(f'{self.sound}')
+
+    def play(self):
+        print('애완동물과 놀기')
+
+pet1 = Dog()
+pet2 = Cat()
+print(Pet(pet1.sound))
+print(pet2.sound)
